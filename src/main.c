09456e92d6e297f82e14b676f15522325fe69518
robin.vidal <robin.vidal.pro@gmail.com>
2025-06-16 00:03:04 +0200

    feat: create isolated process with clone and exec shell
    - use clone() with UTS, NS, PID, IPC, NET namespaces
    - launch shell inside isolated process
    - basic proof of concept for container isolation

--- src/main.c
+++ src/main.c
@@ -1,7 +1,44 @@
+#define _GNU_SOURCE
+#include <sched.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char child_stack[STACK_SIZE];
+
+int child_func(void *arg)
+{
+    (void)arg;
+    printf("Child process: PID=%ld\n", (long)getpid());
+    execlp("sh", "sh", NULL);
+    perror("execlp failed");
+    return 1;
+}
 
 int main(void)
 {
-    printf("Hello from tinydocker!\n");
-    return 0;
+    pid_t pid = clone(child_func, child_stack + STACK_SIZE,
+                      CLONE_NEWUTS | CLONE_NEWNS | CLONE_NEWPID | CLONE_NEWIPC
+                          | CLONE_NEWNET | SIGCHLD,
+                      NULL);
+
+    if (pid == -1)
+    {
+        perror("clone failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Child process created with PID: %d\n", pid);
+
+    if (waitpid(pid, NULL, 0) == -1)
+    {
+        perror("waitpid failed");
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
